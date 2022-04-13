884d718b13997f3398b5cca45b0953326eb6d788
AnhDuc0125 <vuducanh0125@gmail.com>
2022-04-13 20:43:15 +0700

    checkout test

--- pages/home.php
+++ pages/home.php
@@ -5,12 +5,13 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home Page</title>
+    <title>Index Page</title>
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="../assets/css/master.css" />
     <link rel="stylesheet" href="../assets/css/navbar.css" />
     <link rel="stylesheet" href="../assets/css/component.css" />
+    <!-- test github commit -->
 </head>
 
 <body>
