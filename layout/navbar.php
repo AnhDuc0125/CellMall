d502b278236ecb54131948233ecaacbe206e922a
anhduc0125 <vuducanh0125@gmail.com>
2022-04-15 14:47:15 +0700

    fix navbar ui/ux

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,45 +1,34 @@
 <div id="navbar">
-    <nav class="navbar">
-        <ul class="navbar__items">
-            <li class="navbar__item" id="logo">
-                <a href="../pages/home.php" class="item__link">
-                    <h2>foneMart</h2>
-                </a>
-            </li>
-            <li class="navbar__item" id="search">
-                <div class="search__bar">
-                    <input type="text" class="search__field" />
-                    <ion-icon name="search-outline" class="search__icon"></ion-icon>
+    <div class="navbar">
+        <div class="navbar__item">
+            <div class="logo">CellMall</div>
+            <div class="search">
+                <input type="text" class="search__input">
+                <ion-icon name="search-outline" class="search__icon"></ion-icon>
+            </div>
+            <div class="another">
+                <div class="contact">
+                    <ion-icon name="call-outline" class="symbol"></ion-icon>
+                    <div class="content">
+                        <p>Contact Us</p>
+                        <b>2466.2469</b>
+                    </div>
                 </div>
-            </li>
-            <li class="navbar__item" id="contact">
-                <a href="#" class="item__link">
-                    <div class="contact__btn">
-                        <ion-icon name="call-outline" class="contact__icon"></ion-icon>
-                        <div class="contact__text">
-                            <p>Contact Us</p>
-                            <b>1900.0088</b>
-                        </div>
+                <div class="cart">
+                    <ion-icon name="bag-handle-outline" class="symbol"></ion-icon>
+                    <div class="content">
+                        <p>Your Cart</p>
+                        <b>Check Cart</b>
                     </div>
-                </a>
-            </li>
-            <li class="navbar__item" id="cart">
-                <a href="#" class="item__link">
-                    <div class="cart__btn">
-                        <ion-icon name="cart-outline" class="cart__icon"></ion-icon>
-                        <div class="cart__text">
-                            <p>Your Cart</p>
-                        </div>
-                    </div>
-                </a>
-            </li>
-            <li class="navbar__item" id="member">
-                <div class="member__btn">
-                    <ion-icon name="person-circle-outline" class="member__icon"></ion-icon>
-                    <div class="member__text">
+                </div>
+                <div class="cart">
+                    <ion-icon name="person-circle-outline" class="symbol"></ion-icon>
+                    <div class="content">
                         <p>Member</p>
+                        <b>Login/Signup</b>
                     </div>
-            </li>
-        </ul>
-    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
