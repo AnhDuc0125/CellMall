52a36a2c990c1aff2dcc520b35eca9297ae8157c
anhduc0125 <vuducanh0125@gmail.com>
2022-04-19 14:42:03 +0700

    updated navbar

--- layout/navbar.php
+++ layout/navbar.php
@@ -3,14 +3,21 @@
         <div class="navbar__item">
             <a href="home.php" class="logo">CellMall</a>
             <div class="search">
-                <input type="text" class="search__input">
+                <input type="text" class="search__input" placeholder="Search">
                 <ion-icon name="search-outline" class="search__icon"></ion-icon>
             </div>
             <div class="another">
+                <a href="#" class="about">
+                    <ion-icon name="people-circle-outline" class="symbol"></ion-icon>
+                    <div class="content">
+                        <p class="item__title">About Us</p>
+                        <b class="item__detail">About the team</b>
+                    </div>
+                </a>
                 <a href="#" class="contact">
                     <ion-icon name="call-outline" class="symbol"></ion-icon>
                     <div class="content">
-                        <p class="item__title">Contact Us</p>
+                        <p class="item__title">Contact</p>
                         <b class="item__detail">2466.2469</b>
                     </div>
                 </a>
