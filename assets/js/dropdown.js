24b3a39f5bf26ddc58415cdf6255438809755827
AnhDuc0125 <vuducanh0125@gmail.com>
2022-04-23 09:38:21 +0700

    update search page with js

--- assets/js/dropdown.js
+++ assets/js/dropdown.js
@@ -1,8 +1,15 @@
 let dropElements = document.querySelectorAll("div.filter__item");
 let dropDownElements = document.querySelectorAll("div.filter__dropdown");
 
-dropElements.forEach((item) => {
+dropElements.forEach((item, index) => {
   item.onclick = (e) => {
+    for (i = 0; i < dropDownElements.length; i++) {
+      if (i == index) {
+        continue;
+      }
+      dropDownElements[i].classList.remove("active");
+    }
+
     let dropdown = item.querySelector(".filter__dropdown");
     dropdown.classList.toggle("active");
   };
