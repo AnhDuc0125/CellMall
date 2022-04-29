5dd752497eb7241d053c6ed732693dc06f03ab0a
nguyenphananh <phananhnguyen273@gmail.com>
2022-04-29 19:35:18 +0700

    fix admin

--- admin/customers.php
+++ admin/customers.php
@@ -1,4 +1,10 @@
-
+<?php
+   require_once("../database/dbContext.php");
+   require_once("../database/utility.php");
+ 
+   $select = "select * from users";
+   $userList = executeResult($select);
+?>
 <?php session_start(); ?>
 <?php include_once("./templates/top.php"); ?>
 <?php include_once("./templates/navbar.php"); ?>
@@ -19,119 +25,273 @@
             <tr>
               <th>#</th>
               <th>Name</th>
+              <th>Address</th>
               <th>Email</th>
               <th>Mobile</th>
-              <th>Address</th>
+              
+              <th>Action</th>
             </tr>
-          </thead>
-          <tbody id="customer_list">
-            <!-- <tr>
-              <td>1</td>
-              <td>ABC</td>
-              <td>FDGR.JPG</td>
-              <td>122</td>
-              <td>eLECTRONCS</td>
-              <td>aPPLE</td>
-              <td><a class="btn btn-sm btn-info"></a><a class="btn btn-sm btn-danger">Delete</a></td>
-            </tr> -->
-          </tbody>
-        </table>
-      </div>
-    </main>
-  </div>
+            </tr>
+                </thead>
+                <tbody id="customers_list">
+                    <?php
+                      $index = 0;
+                      foreach($userList as $item) {
+                        echo '<tr>
+                                <td>'. ++$index .'</td>
+                                <td>'. $item['fullname'] .'</td>
+                                <td>'.  $item['address'] .'</td>
+                                <td>'. $item['email'] .'</td>
+                                <td>'. $item['phone'] .'</td>
+                  
+                                <td><a style="cursor: pointer" class="btn btn-sm btn-warning"><ion-icon name="brush-outline"></ion-icon></a><a style="cursor: pointer" class="mx-2 btn btn-sm text-light btn-danger"><ion-icon name="trash-outline"></ion-icon></a></td>
+                              </tr>';
+                      }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        </main>
+    </div>
 </div>
 
-<!--modal-->
+
+
+<!-- Add user Modal start -->
 <div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Add user</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
-</div>
-<div class="modal-body">
+            </div>
+            <div class="modal-body">
                 <form method="post" id="add-product-form" enctype="multipart/form-data">
                     <div class="row">
                         <div class="col-12">
                             <div class="form-group">
-                                <label>user name</label>
+                                <label>user Name</label>
                                 <input type="text" name="product_name" class="form-control"
                                     placeholder="Enter Product Name">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>brand Name</label>
+                                <select class="form-control brand_list" name="brand_id">
+                                    <option value="">Select Brand</option>
+                                    <?php
+                                      foreach($brandList as $item) {
+                                        echo '<option value="'. $item['id'] .'">'. $item['name'] .'</option>';
+                                      }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Category Name</label>
+                                <select class="form-control category_list" name="category_id">
+                                    <option value="">Select Category</option>
+                                    <?php
+                                      foreach($cateList as $item) {
+                                        echo '<option value="'. $item['id'] .'">'. $item['name'] .'</option>';
+                                      }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Description</label>
+                                <textarea class="form-control" name="product_desc"
+                                    placeholder="Enter product desc"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Price</label>
+                                <input min="1" type="number" name="product_price" class="form-control"
+                                    placeholder="Enter Product Price">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Oldprice</label>
+                                <input type="number" name="product_oldPrice" class="form-control"
+                                    placeholder="Enter Product OldPrice">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Resolution</label>
+                                <input type="text" name="product_resolution" class="form-control"
+                                    placeholder="Enter Product Resolution">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Image</label>
+                                <input type="text" name="product_image" class="form-control"
+                                    placeholder="Enter Product Image URL">
+                            </div>
+                            </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product discount</label>
+                                <input type="number" name="product_discount" class="form-control"
+                                    placeholder="Enter Product discount">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product status</label>
+                                <input type="text" name="product_status" class="form-control"
+                                    placeholder="Enter Product status">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product storage</label>
+                                <input type="text" name="product_Storage" class="form-control"
+                                    placeholder="Enter Product Storage">
+                            </div>
+                            </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product camera</label>
+                                <input type="number" name="product_Camera" class="form-control"
+                                    placeholder="Enter Product Camera">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product chip</label>
+                                <input type="text" name="product_chip" class="form-control"
+                                    placeholder="Enter Product Chip">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Battery</label>
+                                <input type="text" name="product_Battery" class="form-control"
+                                    placeholder="Enter Product Image URL">
+                        </div>
+                        <input type="hidden" name="add_product" value="1">
+                        <div class="col-12">
+                            <button type="button" class="btn btn-primary add-product">Add Product</button>
+                        </div>
+                    </div>
 
-<!-- Modal -->
-<div class="modal fade" id="add_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form id="add-product-form" enctype="multipart/form-data">
-            <div class="row">
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Product Name</label>
-                        <input type="text" name="product_name" class="form-control" placeholder="Enter Product Name">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Add Product Modal end -->
+
+<!-- Edit Product Modal start -->
+<div class="modal fade" id="edit_product_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Product</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="edit-product-form" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Name</label>
+                                <input type="text" name="e_product_name" class="form-control"
+                                    placeholder="Enter Product Name">
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Brand Name</label>
+                                <select class="form-control brand_list" name="e_brand_id">
+                                    <option value="">Select Brand</option>
+                                    <?php
+                                      foreach($brandList as $item) {
+                                        echo '<option value="'. $item['id'] .'">'. $item['name'] .'</option>';
+                                      }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Category Name</label>
+                                <select class="form-control category_list" name="e_category_id">
+                                    <option value="">Select Category</option>
+                                    <?php
+                                      foreach($cateList as $item) {
+                                        echo '<option value="'. $item['id'] .'">'. $item['name'] .'</option>';
+                                      }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Description</label>
+                                <textarea class="form-control" name="e_product_desc"
+                                    placeholder="Enter product desc"></textarea>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Product Price</label>
+                                <input min="1" type="number" name="e_product_price" class="form-control"
+                                    placeholder="Enter Product Price">
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Brand Name</label>
-                        <select class="form-control brand_list" name="brand_id">
-                            <option value="">Select Brand</option>
-                        </select>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label>Product Old price </label>
+                            <input type="text" name="e_product_oldPrice" class="form-control"
+                                placeholder="Enter Product oldprice">
+                        </div>
                     </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group">
+                    <label>Product Resolution </label>
+                    <input type="text" name="e_product_resolution" class="form-control"
+                        placeholder="Enter Product Resolution">
                 </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Category Name</label>
-                        <select class="form-control category_list" name="category_id">
-                            <option value="">Select Category</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Product Description</label>
-                        <textarea class="form-control" name="product_desc" placeholder="Enter product desc"></textarea>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Product Price</label>
-                        <input type="number" name="product_price" class="form-control" placeholder="Enter Product Price">
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Product Keywords <small>(eg: apple, iphone, mobile)</small></label>
-                        <input type="text" name="product_keywords" class="form-control" placeholder="Enter Product Keywords">
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="form-group">
-                        <label>Product Image <small>(format: jpg, jpeg, png)</small></label>
-                        <input type="file" name="product_image" class="form-control">
-                    </div>
-                </div>
-                <input type="hidden" name="add_product" value="1">
-                <div class="col-12">
-                    <button type="button" class="btn btn-primary add-product">Add Product</button>
+            </div>
+            <div class="col-12">
+                <div class="form-group">
+                    <label>Product Image</label>
+                    <input type="text" name="e_product_image" placeholder="Enter Product Image URL"
+                        class="form-control">
+                    <img src="../product_images/1.0x0.jpg" class="img-fluid" width="50">
                 </div>
             </div>
             
-        </form>
-      </div>
-    </div>
-  </div>
+            <input type="hidden" name="pid">
+            <input type="hidden" name="edit_product" value="1">
+            <div class="col-12">
+                <button type="button" class="btn btn-primary submit-edit-product">Add Product</button>
+            </div>
+        </div>
 
-<!-- Modal -->
+        </form>
+    </div>
+</div>
+</div>
+</div>
+<!-- Edit Product Modal end -->
 
 <?php include_once("./templates/footer.php"); ?>
 
--- admin/js/customers.js
+++ admin/js/customers.js
@@ -17,16 +17,13 @@ $(document).ready(function(){
 					var customersHTML = "";
 
 					$.each(resp.message, function(index, value){
-
-						customersHTML += '<tr>'+
-									          '<td>#</td>'+
-									          '<td>'+value.first_name+' '+value.last_name+'</td>'+
-									          '<td>'+value.email+'</td>'+
-									          '<td>'+value.mobile+'</td>'+
-									          '<td>'+value.address1+'<br>'+value.address2+'</td>'+
-									       '</tr>'
-
+						brandHTML += '<tr>'+
+										'<td></td>'+
+										'<td>'+ value.user_title +'</td>'+
+										'<td><a class="btn btn-sm btn-info edit-brand"><span style="display:none;">'+JSON.stringify(value)+'</span><i class="fas fa-pencil-alt"></i></a>&nbsp;<a bid="'+value.brand_id+'" class="btn btn-sm btn-danger delete-brand"><i class="fas fa-trash-alt"></i></a></td>'+
+									'</tr>';
 					});
+	
 
 					$("#customer_list").html(customersHTML);
 
@@ -39,43 +36,75 @@ $(document).ready(function(){
 		
 	}
 
-	function getCustomerOrders(){
+	$(document.body).on('click', '.edit-product', function(){
+
+		console.log($(this).find('span').text());
+
+		var product = $.parseJSON($.trim($(this).find('span').text()));
+
+		console.log(product);
+
+		$("input[name='e_user_name']").val(product.product_title);
+		$("select[name='e_user_id']").val(product.brand_id);
+		$("select[name='e_phone_id']").val(product.cat_id);
+		$("textarea[name='e_email_desc']").val(product.product_desc);
+		$("#edit_user_modal").modal('show');
+
+	});
+
+	$(".submit-edit-user").on('click', function(){
+
 		$.ajax({
-			url : '../admin/classes/Customers.php',
+
+			url : '../admin/classes/Products.php',
 			method : 'POST',
-			data : {GET_CUSTOMER_ORDERS:1},
+			data : new FormData($("#edit-user-form")[0]),
+			contentType : false,
+			cache : false,
+			processData : false,
 			success : function(response){
-				
 				console.log(response);
 				var resp = $.parseJSON(response);
 				if (resp.status == 202) {
-
-					var customerOrderHTML = "";
-
-					$.each(resp.message, function(index, value){
-
-						customerOrderHTML +='<tr>'+
-								              '<td>#</th>'+
-								              '<td>'+ value.order_id +'</td>'+
-								              '<td>'+ value.product_id +'</td>'+
-								              '<td>'+ value.product_title +'</td>'+
-								              '<td>'+ value.qty +'</td>'+
-								              '<td>'+ value.trx_id +'</td>'+
-								              '<td>'+ value.p_status +'</td>'+
-								            '</tr>';
-
-					});
-
-					$("#customer_order_list").html(customerOrderHTML);
-
+					$("#edit-user-form").trigger("reset");
+					$("#edit_user_modal").modal('hide');
+					getProducts();
+					alert(resp.message);
 				}else if(resp.status == 303){
-					$("#customer_order_list").html(resp.message);
+					alert(resp.message);
+				}
+			}
+
+		});
+
+
+	});
+
+	$(document.body).on('click', '.delete-user', function(){
+
+		var pid = $(this).attr('pid');
+		if (confirm("Are you sure to delete this item ?")) {
+			$.ajax({
+
+				url : '../admin/classes/Products.php',
+				method : 'POST',
+				data : {DELETE_PRODUCT: 1, pid:pid},
+				success : function(response){
+					console.log(response);
+					var resp = $.parseJSON(response);
+					if (resp.status == 202) {
+						getProducts();
+					}else if (resp.status == 303) {
+						alert(resp.message);
+					}
 				}
 
-			}
-		})
+			});
+		}else{
+			alert('Cancelled');
+		}
 		
-	}
 
+	});
 
 });
