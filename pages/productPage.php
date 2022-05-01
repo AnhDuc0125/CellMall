a72f90c52665fa2bcca3373f853a7415bc0157f5
AnhDuc0125 <vuducanh0125@gmail.com>
2022-05-01 18:26:25 +0700

    update product page

--- pages/productPage.php
+++ pages/productPage.php
@@ -16,7 +16,9 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title><?=$product['title']?></title>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="../assets/css/master.css" />
     <link rel="stylesheet" href="../assets/css/navbar.css" />
     <link rel="stylesheet" href="../assets/css/footer.css" />
@@ -32,14 +34,14 @@
     <!-- navbar -->
     <div id="main">
         <div class="product">
-            <h1 class="product__title"><?=$product['title']?></h1>
+            <h2 class="product__title"><?=$product['title']?> | Chính hãng VN/A</h2>
             <div class="product__detail">
                 <div class="detail__left">
                     <img src="<?=$product['image']?>" alt="" class="detail__image" />
                 </div>
-                <div class="product__right">
+                <div class="product__middle">
                     <div class="product__price">
-                        <h1 style="display: inline-block"><?=number_format($product['price'])?> <u>đ</u></h1>
+                        <h2 style="display: inline-block"><?=number_format($product['price'])?> <u>đ</u></h2>
                         | Giá đã bao gồm VAT
                     </div>
 
@@ -49,14 +51,41 @@
                     <div class="product__desc">
                         <?=$product['description']?>
                     </div>
-                    <button class="product__addToCart_btn">ADD TO CART</button>
+                    <button class="product__addToCart_btn">
+                        ADD TO CART
+                    </button>
+                </div>
+                <div class="product__right">
+                    <table>
+                        <caption>
+                            <p>Product's detail</p>
+                        </caption>
+                        <tr>
+                            <td class="icon__detail">
+                                <ion-icon name="phone-portrait-outline"></ion-icon>
+                            </td>
+                            <td>New, full accessories from the manufacturer.</td>
+                        </tr>
+                        <tr>
+                            <td class="icon__detail">
+                                <ion-icon name="cube-outline"></ion-icon>
+                            </td>
+                            <td>USB-C to Lightning cable, guide book.</td>
+                        </tr>
+                        <tr>
+                            <td class="icon__detail">
+                                <ion-icon name="hand-left-outline"></ion-icon>
+                            </td>
+                            <td>1 EXCHANGE 1 in 30 days if there is a manufacturer hardware defect.</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
     <!-- Footer -->
     <?php
-      include_once('../layout/footer.php');
+    //   include_once('../layout/footer.php');
     ?>
 </body>
 
